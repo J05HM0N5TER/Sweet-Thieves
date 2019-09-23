48616202fcef1700d1f8291cb3baa01413c23b13
Josh <42488282+J05HM0N5TER@users.noreply.github.com>
2019-09-23 16:53:12 +1000

    Added controller for the collectables and removed unneeded player controller to prevent confusion.

---
+++ SweetTheives/Assets/Scripts/CollectableController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour
+{
+    public int stackSize = 1;
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerControllerXbox script = other.gameObject.GetComponent<PlayerControllerXbox>();
+            if (script == null)
+            {
+                Debug.Log("No script");
+            }
+            script.heldCollectables += stackSize;
+            if (script.objectHit == this.gameObject)
+            {
+                script.objectHit = null;
+                script.tongueHitCollectible = false;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
