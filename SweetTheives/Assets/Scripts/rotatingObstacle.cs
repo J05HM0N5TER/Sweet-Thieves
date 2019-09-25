5cdb389c5136528b6208e9014e278e27751932e2
furbet1992 <48895389+furbet1992@users.noreply.github.com>
2019-09-25 23:31:32 +1000

    added script called rotating Obstacles and have applied them to the rotating objects in the centre

---
+++ SweetTheives/Assets/Scripts/rotatingObstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatingObstacle : MonoBehaviour
+{
+    public int orbitSpeed; 
+
+
+    void Update()
+    {
+        orbiting(); 
+    }
+
+    // Update is called once per frame
+    void orbiting()
+    {
+        transform.Rotate(Vector3.up * orbitSpeed * Time.deltaTime);
+    }
+}
