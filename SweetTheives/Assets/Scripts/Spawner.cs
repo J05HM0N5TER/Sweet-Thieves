534c29852f5cf61cbe710ba375ee631fb1c2bdbb
furbet1992 <48895389+furbet1992@users.noreply.github.com>
2019-10-07 14:31:46 +1100

    spawner script

---
+++ SweetTheives/Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnee;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+    public int maxSpawn;
+    int spawned;
+    // Start is called before the frst frame update
+    void Start()
+    {
+        spawned = 0;
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+    
+    public void SpawnObject()
+    {
+        //for(int i = 0; i  <= maxSpawn; i ++)
+        //{
+           
+        //}
+        Instantiate(spawnee, transform.position, transform.rotation);
+        spawned += 1;
+        if (spawned == maxSpawn)
+        {
+            CancelInvoke("SpawnObject");
+        }
+
+
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
