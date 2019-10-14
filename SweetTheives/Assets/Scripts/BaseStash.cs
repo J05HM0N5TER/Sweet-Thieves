cfd12de105e53b2e1653e62f8b1d6c88e7cf786e
furbet1992 <48895389+furbet1992@users.noreply.github.com>
2019-10-14 13:44:31 +1100

    bashStash script

--- SweetTheives/Assets/Scripts/BaseStash.cs
+++ SweetTheives/Assets/Scripts/BaseStash.cs
@@ -4,34 +4,40 @@ using UnityEngine;
 
 public class BaseStash : MonoBehaviour
 {
-     public float stashSize;
-    [SerializeField] GameObject player;
-    [SerializeField] Spawner spawner;
+    // size of current stash
+     public float stashSize = 0;
+    // what player is connected to base
+    [SerializeField] Transform player = null;
+    // what spawner is connected to base
+    [SerializeField] Spawner spawner = null;
+    // delay between spawning pancakes into base
     [SerializeField] float delay = 0.0f;
-    // [SerializeField] GameObject spawnee;
-    private void Start()
-    {
-        
-    }
 
+    // on collision of base
     private IEnumerator OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        //if it is the players base do code, if not nothing happens
+        if (other.gameObject.transform == player)
         {
-            PlayerControllerXbox player = other.gameObject.GetComponent<PlayerControllerXbox>();
+            //getting script from PlayerControllerXbox
+            PlayerControllerXbox playercontrol = other.gameObject.GetComponent<PlayerControllerXbox>();
             
-            if (player == null )
+            //if no script found load error
+            if (playercontrol == null )
             {
                 Debug.Log("No script");
             }
+            // otherwise
             else
             {
-                //Spawner spawner = other.gameObject.GetComponent<Spawner>();
-               // stashSize += player.heldCollectables;
-                spawner.maxSpawn += player.heldCollectables;
-                if(player.heldCollectables != 0)
+                // the max allowed to spawn in this instance is equal to how mnay pancakes the player is holding
+                spawner.maxSpawn += playercontrol.heldCollectables;
+                
+                //if the player is holding more than 0
+                if(playercontrol.heldCollectables != 0)
                 {
-                    for (int i = 0; i < player.heldCollectables; i++)
+                    //spawn the amount the player is holding with frozen transforms and a delay between spawns
+                    for (int i = 0; i < playercontrol.heldCollectables; i++)
                     {
                         spawner.SpawnObject().GetComponent<Rigidbody>().constraints =
                           RigidbodyConstraints.FreezePositionX |
@@ -42,10 +48,9 @@ public class BaseStash : MonoBehaviour
                     }
                    
                 }
-                player.heldCollectables = 0;
+                // player loses how many is being held and the max spawn is returned to 0.
+                playercontrol.heldCollectables = 0;
                 spawner.maxSpawn = 0;
-
-
 
             }
            
