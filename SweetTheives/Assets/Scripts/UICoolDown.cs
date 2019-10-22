16ce78e5ed9bad393f5dc988ebaba3f888729614
Lenammon <40161201+Lenammon@users.noreply.github.com>
2019-10-22 15:18:20 +1100

    UI coolDown for the tongue

---
+++ SweetTheives/Assets/Scripts/UICoolDown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UICoolDown : MonoBehaviour
+{
+    //this is the image of the bar that is filling up 
+    public Image coolDownImage;
+
+    public GameObject player;
+  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControllerXbox playerscript = player.GetComponent<PlayerControllerXbox>();//max
+        float maxCoolDownTime  = playerscript.tongueCooldown;
+        float currentCoolDownTime = playerscript.currentCooldown;
+        
+        if (currentCoolDownTime > 0f)
+        {
+            coolDownImage.fillAmount = 1 - (currentCoolDownTime / maxCoolDownTime);
+        }
+        else
+        {
+            coolDownImage.fillAmount = 1;
+        }
+    }
+}
