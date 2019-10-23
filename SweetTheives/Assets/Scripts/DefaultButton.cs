0584ff59fa76e86c79a1ec7cdc3a58286ef0933e
Lenammon <40161201+Lenammon@users.noreply.github.com>
2019-10-23 15:50:52 +1100

    added a main menu,settings, credits, Fixed UI for the tongue and the quit button.

---
+++ SweetTheives/Assets/Scripts/DefaultButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DefaultButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+   //this to make sure that there is always a button selected and 
+   // so when the player presses the back button it defaults to the play button
+    public GameObject defaultButton = null;
+    void Start()
+    {
+        
+    }
+    //this to make sure that when the scene changes to the default button of that scence 
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(defaultButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this make sure that if a plyer clicks out it will back to the defualt button 
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(defaultButton);
+        }
+    }
+}
--- SweetTheives/Assets/Scripts/SceneLoader.cs
+++ SweetTheives/Assets/Scripts/SceneLoader.cs
@@ -1,27 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using XboxCtrlrInput;
 
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] XboxButton selectbutton = XboxButton.A;
+
+    //public GameObject defaultButton = null;
+    
     //public void PlayGame()
     //{
    //     SceneManager.LoadScene(1);
 
     //}
-    public void Update()
+    public void PlayGame()
     {
-       
         if (XCI.GetButton(selectbutton))
         {
-            SceneManager.LoadScene(1);
+            // this will add scences in the same order you put it in build settings
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+    }
+    public void Update()
+    {
+        //if(EventSystem.current.currentSelectedGameObject == null)
+        //    EventSystem.current.SetSelectedGameObject(defaultButton);
+
+        //    if (XCI.GetButton(selectbutton))
+        //    {
+        //        SceneManager.LoadScene(1);
+        //    }
     }
     public void QuitGame()
     {
+        Debug.Log("quit!");
         Application.Quit(); 
     }
 }
