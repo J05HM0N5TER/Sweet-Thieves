cd6b19e71055bb8335a2c81879bd802735586b0c
ironclad1875 <47731595+ironclad1875@users.noreply.github.com>
2019-11-04 15:59:23 +1100

    Made a button that works as a Venus fly trap, respawns an enemy player thats in the players base

---
+++ SweetTheives/Assets/Scripts/VenusButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VenusButton : MonoBehaviour
+{
+
+    [SerializeField] GameObject Player = null;
+    [SerializeField] GameObject PlayerBase = null;
+    [SerializeField] float distroyRadius = 0.0f; 
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Player)
+        { 
+            Collider[] hitColliders = Physics.OverlapSphere(PlayerBase.transform.position, distroyRadius);
+            foreach (Collider current in hitColliders)
+            {
+                if (current.gameObject.tag == "Player")
+                {
+                    PlayerControllerXbox playercontrol = current.gameObject.GetComponent<PlayerControllerXbox>();
+                    playercontrol.ResetToSpawn();
+                }
+            }     
+        }
+    }
+}
