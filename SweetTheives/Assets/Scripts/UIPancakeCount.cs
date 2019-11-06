b8af1e7b93c4d2f082c44b32ca590db6a36517da
Lenammon <40161201+Lenammon@users.noreply.github.com>
2019-11-06 16:06:31 +1100

    added all the menu buttons, started working on an indication of how many pancakes can be picked up.

---
+++ SweetTheives/Assets/Scripts/UIPancakeCount.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPancakeCount : MonoBehaviour
+{
+    public Image pancakeCount;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this is very broken dont worry about this yet
+        PlayerControllerXbox playerscripter = player.GetComponent<PlayerControllerXbox>();
+        int maxPancake = playerscripter.maxHeldCollectables;
+        int currentPancake = playerscripter.heldCollectables;
+         
+        if(currentPancake > 0)
+        {
+            
+            pancakeCount.fillAmount = 1 - (currentPancake / maxPancake);
+        }
+        else
+        {
+            pancakeCount.fillAmount = 1;
+        }
+    }
+}
