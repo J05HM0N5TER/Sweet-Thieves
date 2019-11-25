92afdd8f4d40b78b35f23856e8b66180ad670463
Josh <42488282+J05HM0N5TER@users.noreply.github.com>
2019-11-26 09:09:02 +1100

    Updated LightFlicker so that it is always on if there is a pancake at the base and the light isn't flashing.

--- SweetTheives/Assets/Scripts/LightFlicker.cs
+++ SweetTheives/Assets/Scripts/LightFlicker.cs
@@ -4,65 +4,61 @@ using UnityEngine;
 
 public class LightFlicker : MonoBehaviour
 {
-    public GameObject player;
-    private PlayerControllerXbox playerController;
+	public GameObject player;
+	private PlayerControllerXbox playerController;
 
-    public GameObject Base;
-    private BaseStash baseStashAmount;
+	public GameObject Base;
+	private BaseStash baseStashAmount;
 
-    public float flickerTime = 0.5f;
+	public float flickerTime = 0.5f;
 
-    public int amountFlicker = 2;
+	public int amountFlicker = 2;
 
-    private float currentTimer = 0f;
+	private float currentTimer = 0f;
 
-    private bool firstTime = true;
-    private bool playerFull = false;
-    private int currentAmountFlickerd = 0;
+	private bool firstTime = true;
+	private bool playerFull = false;
+	private int currentAmountFlickerd = 0;
 
-    void Start()
-    {
-        playerController = player.GetComponent<PlayerControllerXbox>();
-        baseStashAmount = Base.GetComponent<BaseStash>();
-    }
+	void Start()
+	{
+		playerController = player.GetComponent<PlayerControllerXbox>();
+		baseStashAmount = Base.GetComponent<BaseStash>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-        // Player is full
-        if (playerController.heldCollectables >0) //playerController.maxHeldCollectables == playerController.heldCollectables)
-        {
-            // Set timer
-            currentTimer += Time.deltaTime;
-            playerFull = true;
-            //gameObject.GetComponent<Light>().enabled = true;
-            // If timer is up or its the first time (where it will do it constantly)
-            if (currentTimer > flickerTime && (currentAmountFlickerd < amountFlicker || firstTime))
-            {
-                currentAmountFlickerd++;
-                Light temp = gameObject.GetComponent<Light>();
-                temp.enabled = !temp.enabled;
-                currentTimer = 0f;
-            }
-        }
-        else
-        {
-            // If the player dropped them off
-            if (playerFull)
-            {
-                firstTime = false;
-                currentAmountFlickerd = 0;
-            }
-            // Light on when no pancakes at base
-            if (baseStashAmount.stashSize < 1)
-            {
-                gameObject.GetComponent<Light>().enabled = false;
-            }
-            else
-            {
-                gameObject.GetComponent<Light>().enabled = true;
-            }
-        }
-    }
+		// Player is holding pancakes
+		if (playerController.heldCollectables > 0 && currentTimer > flickerTime && (currentAmountFlickerd < amountFlicker || firstTime))
+		{
+			// Set timer
+			currentTimer += Time.deltaTime;
+			playerFull = true;
+			// If timer is up or its the first time (where it will do it constantly)
+			currentAmountFlickerd++;
+			Light temp = gameObject.GetComponent<Light>();
+			temp.enabled = !temp.enabled;
+			currentTimer = 0f;
+		}
+		else
+		{
+			// If the player dropped them off
+			if (playerFull)
+			{
+				firstTime = false;
+				currentAmountFlickerd = 0;
+			}
+			// Light on when no pancakes at base
+			if (baseStashAmount.stashSize < 1)
+			{
+				gameObject.GetComponent<Light>().enabled = false;
+			}
+			else
+			{
+				gameObject.GetComponent<Light>().enabled = true;
+			}
+		}
+	}
 }
