a58d4c00615fb22521367cce41ca89360adb8bde
Josh <42488282+J05HM0N5TER@users.noreply.github.com>
2019-11-25 11:32:48 +1100

    Updated timer to use player names.

--- SweetTheives/Assets/Scripts/TimerRestartGame.cs
+++ SweetTheives/Assets/Scripts/TimerRestartGame.cs
@@ -7,117 +7,118 @@ using UnityEngine.UI;
 using XboxCtrlrInput;
 
 
+
+
 public class TimerRestartGame : MonoBehaviour
 {
-    //text to display the clock
-    public Text clock = null;
-    public float timer = 0.0f;
+	//text to display the clock
+	public Text clock = null;
+	// The timer that counts down
+	public double timer = 0.0f;
 
-    public Text winner = null;
-    private float highestStack = 0;
-    private int winningPlayer = 0;
-    private bool bullshit = false;
-    [SerializeField] BaseStash player1 = null;
-    [SerializeField] BaseStash player2 = null;
-    [SerializeField] BaseStash player3 = null;
-    [SerializeField] BaseStash player4 = null;
-    //[SerializeField] PlayerControllerXbox player = null;
-   // [SerializeField] XboxButton selectbutton = XboxButton.A;
-  //  [SerializeField] XboxButton exitbutton = XboxButton.B;
-    [SerializeField] Canvas winscreen = null;
-   
+	// Where it is displaying the winner
+	public Text winnerText = null;
+	// The array of all the players
+	private BaseStash[] playerBases = new BaseStash[4];
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        winscreen.GetComponent<Canvas>().enabled = false;
-    }
+	public List<GameObject> winningPlayers = new List<GameObject>();
 
-    // Update is called once per frame
-    void Update()
-    {
+	// The canvas that is being displayed when the timer is up
+	[SerializeField] Canvas winscreen = null;
 
-        timer -= Time.deltaTime;
+	private bool finnished = false;
 
-        if (timer <= 0)
-        {
-            timer = 0;
-            //You can restart the scene here
-            
-        }
-        int minutes = (int)(timer / 60);
-        int seconds = (int)timer % 60;
-        //if the seconds in the timer is 9 or less put a ":0"
-        if (seconds <= 9)
-        {
-            clock.text = minutes.ToString() + ":0" + seconds.ToString();
+	// Start is called before the first frame update
+	void Start()
+	{
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		for (int i = 0; i < players.Length; i++)
+		{
+			playerBases[i] = players[i].GetComponent<PlayerControllerXbox>().GetBase().GetComponent<BaseStash>();
+		}
 
-        }
-        //if the seconds is 0 put ":00"
-        else if (seconds == 0)
-        {
-            clock.text = minutes.ToString() + ":00" ;
+		winscreen.GetComponent<Canvas>().enabled = false;
+	}
 
-        }
-        else
-        {
-            clock.text = minutes.ToString() + ":" + seconds.ToString();
-        }
-        if(timer == 0)
-        {
-            Time.timeScale = 0;
-            
-           
-            
-            if(player1.stashSize >= highestStack)
-            {
-                highestStack = player1.stashSize;
-                winningPlayer = 1;
-            }
-            if(player2.stashSize > highestStack)
-            {
-                highestStack = player2.stashSize;
-                winningPlayer = 2;
-            }
-            if (player3.stashSize > highestStack)
-            {
-                highestStack = player3.stashSize;
-                winningPlayer = 3;
-            }
-            if (player4.stashSize > highestStack)
-            {
-                highestStack = player4.stashSize;
-                winningPlayer = 4;
-            }
-            
-            if (bullshit == false)
-            {
-                winner.text = "Player " + winningPlayer + " Won!";
-                winscreen.GetComponent<Canvas>().enabled = true;
-                EventSystem.current.SetSelectedGameObject(null);
-                enabled = false;
-                bullshit = true;
-            }
+	// Update is called once per frame
+	void Update()
+	{
+		// If the winner has been found then don't execute code.
+		if (finnished)
+		{
+			return;
+		}
 
-           
-            ////lena
-            //Debug.Log("loadin winnig screen");
-            //SceneManager.LoadScene("WIN SCREEN");
-            //---------------
+		// Count down timer
+		timer -= Time.deltaTime;
 
+		// Convert timer to display
+		int minutes = (int)(timer / 60);
+		int seconds = (int)timer % 60;
+		//if the seconds in the timer is 9 or less put a ":0"
+		if (seconds <= 9)
+		{
+			clock.text = minutes + ":0" + seconds;
+		}
+		//if the seconds is 0 put ":00"
+		else if (seconds == 0)
+		{
+			clock.text = minutes.ToString() + ":00";
 
-            //if (XCI.GetButton(selectbutton))
-            //{
-             //   Time.timeScale = 1;
-            //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            //}
-            //if (XCI.GetButton(exitbutton))
-            //{
-            //    Application.Quit();
-            //}
-        }
-        
-        
-    }
-   
+		}
+		else
+		{
+			clock.text = minutes + ":" + seconds;
+		}
+
+		// If timer is finished
+		if (timer <= 0)
+		{
+
+			// stop time
+			Time.timeScale = 0;
+
+			int highestStack = int.MinValue;
+
+			// Loop though all the players and find the winner/s
+			for (int i = 0; i < playerBases.Length; i++)
+			{
+				// And find who won
+				if (playerBases[i].stashSize > highestStack)
+				{
+					highestStack = (int)playerBases[i].stashSize;
+
+					// Reset winning players
+					winningPlayers.Clear();
+					// Set to current player
+					winningPlayers.Add(playerBases[i].player.gameObject);
+				}
+				else if (playerBases[i].stashSize == highestStack)
+				{
+					// Add current player to the list of winners
+					winningPlayers.Add(playerBases[i].player.gameObject);
+				}
+			}
+
+			// Create what to write as the winner/s
+			string winString = "";
+			for (int i = 0; i < winningPlayers.Count; i++)
+			{
+				if (i == 0)
+				{
+					winString = winningPlayers[i].name;
+				}
+				else
+				{
+					winString += " & " + winningPlayers[i].name;
+				}
+			}
+
+			// Set text to show who won
+			winnerText.text = winString + " Won!";
+			winscreen.GetComponent<Canvas>().enabled = true;
+			EventSystem.current.SetSelectedGameObject(null);
+			finnished = true;
+		}
+	}
 }
