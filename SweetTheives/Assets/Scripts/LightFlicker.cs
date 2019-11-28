16f5803b2f8e69d8e9a8f3f80be68cf9aae3f395
Joshua Francis <joshua.francis@live.com.au>
2019-11-28 15:34:56 +1100

    Updated LightFlicker.cs.
    Updated commeting and renamed a couple varibles.

--- SweetTheives/Assets/Scripts/LightFlicker.cs
+++ SweetTheives/Assets/Scripts/LightFlicker.cs
@@ -5,23 +5,37 @@ using UnityEngine;
 [RequireComponent(typeof(Light))]
 public class LightFlicker : MonoBehaviour
 {
+	// The player that is using this base
 	public GameObject player;
+	// The script on the player
 	private PlayerControllerXbox playerController;
 
+	// The gameObject for the base that the light is connected to
 	public GameObject Base;
+	// The script that is on the base that stores how many collectables is at the base
 	private BaseStash baseStashAmount;
 
+	// How fast it flickers
 	public float flickerTime = 0.5f;
 
+	// How much it flickers each timer the player picks up a collectable (Other then the first timer)
 	public int amountFlicker = 2;
 
+	// The current timer for how long since last light change
 	private float currentTimer = 0f;
 
+	// Is it the first time in the game since the player picked up a collectable
 	private bool firstTime = true;
-	private bool playerFull = false;
+	// Is the player holding a collectable
+	private bool playerHolding = false;
+	// How many times has the light flickered since the player picked up anything
 	private int currentAmountFlickerd = 0;
+	// The light that is being controlled
     private Light flickeringLight = null;
 
+	/// <summary>
+	/// Cache the things that are expensive to call
+	/// </summary>
 	void Start()
 	{
 		playerController = player.GetComponent<PlayerControllerXbox>();
@@ -29,16 +43,17 @@ public class LightFlicker : MonoBehaviour
         flickeringLight = GetComponent<Light>();
 	}
 
-	// Update is called once per frame
+	/// <summary>
+	/// Deals with all interaction
+	/// </summary>
 	void Update()
 	{
-
         currentTimer += Time.deltaTime;
 		// If the light should be flickering
 		if (playerController.heldCollectables > 0 && (currentAmountFlickerd < amountFlicker || firstTime))
 		{
             // Set timer
-            playerFull = true;
+            playerHolding = true;
             if (currentTimer > flickerTime)
             {
                 // If timer is up or its the first time (where it will do it constantly)
@@ -51,7 +66,7 @@ public class LightFlicker : MonoBehaviour
 		else if (currentTimer > flickerTime)
 		{
 			// If the player dropped them off
-			if (playerFull && playerController.heldCollectables == 0)
+			if (playerHolding && playerController.heldCollectables == 0)
 			{
 				firstTime = false;
 				currentAmountFlickerd = 0;
