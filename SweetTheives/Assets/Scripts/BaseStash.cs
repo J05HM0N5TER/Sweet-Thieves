682b049a175773b295fae3886fffdf89a1eb29ba
Joshua Francis <joshua.francis@live.com.au>
2019-11-28 15:25:42 +1100

    Updated BasePoint.cs and BaseStash.cs.

--- SweetTheives/Assets/Scripts/BaseStash.cs
+++ SweetTheives/Assets/Scripts/BaseStash.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BaseStash : MonoBehaviour
 {
+#pragma warning disable IDE0044 // Add readonly modifier
 
     //text to display the points
     public Text points = null;
@@ -13,10 +14,11 @@ public class BaseStash : MonoBehaviour
     public Transform player = null;
     // what spawner is connected to base
     [SerializeField] Spawner spawner = null;
-    // delay between spawning pancakes into base
-    [SerializeField] float delay = 0.0f;
 
-    [SerializeField] GameObject spawnee = null;
+	// delay between spawning pancakes into base
+	[SerializeField] private float delay = 0.0f;
+
+	[SerializeField] GameObject spawnee = null;
     //stop it from spawning in editor
     [SerializeField] bool stopSpawning = false;
     //will it continue to spawn 
@@ -29,26 +31,33 @@ public class BaseStash : MonoBehaviour
     public int maxSpawn = 0;
     //amount that has been spawned
     int spawned = 0;
-    //is this spawner part of a base
-    //[SerializeField] bool isbase = false;
 
 	public float spawneeHeight;
+#pragma warning restore IDE0044 // Add readonly modifier
 
+	/// <summary>
+	/// Used to cache expensive calls
+	/// </summary>
 	private void Start()
 	{
 		// Get the height of the collectable to use to stack them later
 		spawneeHeight = spawnee.GetComponent<CapsuleCollider>().bounds.size.y;
 	}
 
-	//update function
-	//set the score to stashsize;
+	/// <summary>
+	/// Updates the display for the amount of collectables at the base
+	/// </summary>
 	void Update()
     {
         points.text = stashSize.ToString();
     }
 
 
-    // on collision of base
+    /// <summary>
+	/// Deals with interaction between player and base
+	/// </summary>
+	/// <param name="other">The collider that is colliding with the gameObject</param>
+	/// <returns>Used for the WaitForSeconds function</returns>
     private IEnumerator OnTriggerEnter(Collider other)
     {
 
@@ -86,6 +95,11 @@ public class BaseStash : MonoBehaviour
             }
         }
     }
+
+	/// <summary>
+	/// Spawns a new collectable at the position of that gameObject.
+	/// </summary>
+	/// <returns>The collectable that was spawned</returns>
     public GameObject SpawnObject()
     {
         // spawns the objects
